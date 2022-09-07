8bb0934eecbf3b59b68a524f98b1201aff86624f
Chance <chanceusc@gmail.com>
2022-09-06 21:17:03 -0400

    Impl sort.Interface for text.Texts
    * implements sort.Interface for text.Texts

--- text/text.go
+++ text/text.go
@@ -29,6 +29,7 @@
 package text
 
 import (
+	"sort"
 	"strings"
 	"unicode"
 
@@ -38,9 +39,15 @@ import (
 
 type Texts []Text
 
-func (t Texts) Len() int {
-	return len(t)
-}
+// Less reports whether the element with index i
+// must sort before the element with index j.
+func (t Texts) Less(i int, j int) bool { return t[i] < t[j] }
+
+// Swap swaps the elements with indexes i and j.
+func (t Texts) Swap(i int, j int) { t[i], t[j] = t[j], t[i] }
+
+// Len is the number of elements in t.
+func (t Texts) Len() int { return len(t) }
 
 // TotalLen returns the sum len of each Text in t.
 func (t Texts) TotalLen() int {
@@ -493,3 +500,5 @@ func collect(slice []string) Texts {
 	}
 	return res
 }
+
+var _ sort.Interface = (*Texts)(nil)
